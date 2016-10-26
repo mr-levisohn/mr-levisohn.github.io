5243e8dd4e9f280a1cfb0377debbd909777a135b
mr-levisohn <Levisohns@Evs-MacBook-Pro.local>
2016-10-26 17:55:06 +1100

    form upload

--- js/custom.js
+++ js/custom.js
@@ -37,6 +37,137 @@ $(window).on('scroll', function(){
 //
 
 
+// $('.gallery').featherlightGallery();
+    // previousIcon: '«',
+    // nextIcon: '»',
+    // galleryFadeIn: 300,
+
+    // openSpeed: 300
+// });
+
+
+
+//form //
+
+    $('#contact_form').bootstrapValidator({
+
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            first_name: {
+                validators: {
+                        stringLength: {
+                        min: 2,
+                    },
+                        notEmpty: {
+                        message: 'Please supply your first name'
+                    }
+                }
+            },
+             last_name: {
+                validators: {
+                     stringLength: {
+                        min: 2,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your last name'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply your email address'
+                    },
+                    emailAddress: {
+                        message: 'Please supply a valid email address'
+                    }
+                }
+            },
+            phone: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply your phone number'
+                    },
+                    phone: {
+                        country: 'AUS',
+                        message: 'Please supply a vaild phone number with area code'
+                    }
+                }
+            },
+            address: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your street address'
+                    }
+                }
+            },
+            city: {
+                validators: {
+                     stringLength: {
+                        min: 4,
+                    },
+                    notEmpty: {
+                        message: 'Please supply your city'
+                    }
+                }
+            },
+            state: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please select your state'
+                    }
+                }
+            },
+            post: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please supply your post code'
+                    },
+                    zipCode: {
+                        country: 'AUS',
+                        message: 'Please supply a vaild post code'
+                    }
+                }
+            },
+            comment: {
+                validators: {
+                      stringLength: {
+                        min: 5,
+                        max: 200,
+                        message:'Please enter at least 5 characters and no more than 200'
+                    },
+                    notEmpty: {
+                        message: 'leave us some feedback. Please. come on. you know you want to..'
+                    }
+                    }
+                }
+            }
+        })
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
+
 $('.gallery').featherlightGallery();
     // previousIcon: '«',
     // nextIcon: '»',
